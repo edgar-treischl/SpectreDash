c5abbb762230e6fd964b7b8792f520f4fd22b7c0
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-08-30 11:33:36 +0200

    poetry init

---
+++ src/spectredash/math_tools.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def squared_array(arr):
+    """
+    Squares each element in a NumPy array.
+
+    Parameters:
+        arr (numpy.ndarray): Input array.
+
+    Returns:
+        numpy.ndarray: Array with each element squared.
+    """
+    return np.square(arr)
---
+++ test_run.py
@@ -0,0 +1,6 @@
+from spectredash import squared_array
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+print("Original:", arr)
+print("Squared:", squared_array(arr))
