ea9805bc83833a8f01bfd862b0580adcb6d770c7
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-08-31 11:35:10 +0200

    about without reactive

--- src/spectredash/app.py
+++ src/spectredash/app.py
@@ -2,7 +2,7 @@ import seaborn as sns
 from shiny import App, render, ui
 from spectredash.getduck import duckdb_table
 from spectredash.tables import table_overview
-
+from pathlib import Path
 
 
 df = sns.load_dataset('penguins')  # Replace with your dataset name if needed
@@ -11,28 +11,48 @@ df = sns.load_dataset('penguins')  # Replace with your dataset name if needed
 # The contents of the first 'page' is a navset with two 'panels'.
 page1 = ui.navset_card_underline(
     ui.nav_panel(
+        "🔫 About",
         ui.row(
             ui.column(
                 5,
                 ui.div(
                     ui.p(
-                        "SpectreApp silently infiltrates the OddJob repository, extracting validation results "
+                        "Spectre silently infiltrates the OddJob repository, extracting validation results "
                         "and decoding them into sleek visual intel. Validation results and pointer data create "
                         "a rich meta data for a complete audit trail."
                     ),
-                    ui.p("The next table gives an overview of the most recent validation runs:"),
-                    ui.input_select("datax", "Select Data", choices=["mtcars", "iris"]),
-                    ui.input_select("var", "Select variable", choices=["bill_length_mm", "body_mass_g"])
+                    ui.p("The table on the right side gives an overview of the most recent validation runs. Pick a table name (and version) and SpectreApp visualize key insights"),
                     #ui.hr(),
+                    ui.p("Furthermore, this app includes:"),
+                    ui.tags.ul(
+                        ui.tags.li(ui.strong("Overview:"), " An overview based on the pointer file."),
+                        ui.tags.li(ui.strong("Validation:"), " The validation report created with Octopussy."),
+                        ui.tags.li(ui.strong("Variable Matrix:"), " Displays which variables the data includes."),
+                        ui.tags.li(ui.strong("Class Matrix:"), " Depicts which classes the data includes."),
+                        ui.tags.li(ui.strong("Label Matrix:"), " Shows which labels the data includes.")
+                        ),
+                        ui.p("Created with ❤️, shiny, and ",
+                        ui.tags.img(src="octo.png", height="32px", class_="me-2")
+                        )
                 ),
             ),
             ui.column(
                 7,
+                ui.h4("Select the intel for:", class_="text-muted small"),
+                ui.row(
+                    ui.column(6,
+                    ui.input_select("datax", "Data", choices=["mtcars", "iris"])
+                    ),
+                    ui.column(6,
+                    ui.input_select("var", "Version (optional)", choices=["bill_length_mm", "body_mass_g"])
+                    ),
+                    ui.p("Monitoring data made simple with spectre.", class_="text-muted small")
+                    ),
+                ui.hr(),
                 ui.output_ui("table_html")
             ),
         )
-    ),
-    title="Data Pointers"
+    )
 )
 
 
@@ -46,11 +66,17 @@ page2 = ui.navset_card_underline(
     title="Penguins data",
 )
 
+
 app_ui = ui.page_navbar(
     #ui.nav_spacer(),
-    ui.nav_panel("Bla", page1),
+    ui.nav_panel("About", page1),
     ui.nav_panel("More", page2),
-    title="Spectr",
+    title=ui.span(
+        ui.img(src="logo.png", height="60px", class_="me-2"),
+        "SpectreApp"
+    ),
+    footer = ui.p(ui.tags.a("🎩 Visit the OddJob Repository", href="https://gitlab.lrz.de/edgar-treischl/OddJob", target="_blank")
+)
 )
 
 
@@ -78,5 +104,6 @@ def app_server(input, output, session):
         
         return html
 
+www_dir = Path(__file__).parent / "www"
+app = App(app_ui, app_server, static_assets=www_dir)
 
-app = App(app_ui, app_server)
